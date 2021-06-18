33cba07c9cbef7b6bb52fdc488b87fd86cad9a23
aki <simizuakiralmn@gmail.com>
2021-06-18 23:42:11 +0900

    in ss_1

---
+++ ss_1/e_1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+
+
+char input[10];
+
+// scanf()
+
+scanf("%s", input);
+
+printf("%s\n", input);
+
+
+return 0;
+
+}
---
+++ ss_1/e_2.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+
+int main(){
+
+char input[10];
+
+scanf_s("%s", input);
+
+
+printf("%s\n",input);
+
+
+return 0;
+
+}
